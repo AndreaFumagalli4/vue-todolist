01f1f52acf91ecd929fc9cb3ac01cac9718cf0ee
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-12-14 14:37:20 +0100

    print all li element with vue

--- js/script.js
+++ js/script.js
@@ -4,7 +4,30 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            
+            shoppingList : [
+                {
+                    text : 'riso',
+                    done : false
+                },
+                {
+                    text : 'pasta',
+                    done : false
+                },
+                {
+                    text : 'pane',
+                    done : false
+                },
+                {
+                    text : 'cereali',
+                    done : false
+                },
+                {
+                    text : 'carne',
+                    done : false
+                },
+            ],
         }
-    }
+    },
+
+    
 }).mount('#app');
